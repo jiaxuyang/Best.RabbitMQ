560de1993db1c7db66ab24074e9e74c078092f79
Shi Jiyong <shijiyong@hotmail.com>
2017-04-12 18:26:15 +0800

    nuget symbols package include full pdb & source

---
+++ msbuild_pack_release.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\amd64\MSBuild.exe" /t:pack /p:Configuration=Release /p:IncludeSymbols=true;IncludeSource=true src\Best.RabbitMQ\Best.RabbitMQ.csproj
